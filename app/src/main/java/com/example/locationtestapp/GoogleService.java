c62fde90e49f589b1eb449265d6441aaa8771316
Ronak <ronak.upadhyay@techgrains.com>
2019-09-03 19:13:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/locationtestapp/GoogleService.java
@@ -0,0 +1,170 @@
+package com.example.locationtestapp;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by deepshikha on 24/11/16.
+ */
+
+public class GoogleService extends Service implements LocationListener {
+
+    boolean isGPSEnable = false;
+    boolean isNetworkEnable = false;
+    double latitude, longitude;
+    LocationManager locationManager;
+    Location location;
+    private Handler mHandler = new Handler();
+    private Timer mTimer = null;
+    long notify_interval = 5000;
+    public static String str_receiver = "servicetutorial.service.receiver";
+    Intent intent;
+    public String TAG = "GoogleService";
+
+
+    public GoogleService() {
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTaskToGetLocation(), 5, notify_interval);
+        intent = new Intent(str_receiver);
+//        fn_getlocation();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void fn_getlocation() {
+        locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworkEnable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!isGPSEnable && !isNetworkEnable) {
+            Toast.makeText(this, "NO INTERNET AND GPS", Toast.LENGTH_SHORT).show();
+        } else {
+
+            if (isNetworkEnable) {
+                location = null;
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    Activity#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for Activity#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, this);
+                if (locationManager!=null){
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (location!=null){
+
+                        Log.e("latitude",location.getLatitude()+"");
+                        Log.e("longitude",location.getLongitude()+"");
+
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        fn_update(location);
+                    }
+                }
+
+            } else {
+                Toast.makeText(this, "NO NOETWORK", Toast.LENGTH_SHORT).show();
+            }
+
+
+            if (isGPSEnable){
+                location = null;
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0,this);
+                if (locationManager!=null){
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location!=null){
+                        Log.e("latitude",location.getLatitude()+"");
+                        Log.e("longitude",location.getLongitude()+"");
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        fn_update(location);
+                    }
+                }
+            } else {
+                Toast.makeText(this, "NO GPS", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+
+    }
+
+    private class TimerTaskToGetLocation extends TimerTask{
+        @Override
+        public void run() {
+
+            mHandler.post(new Runnable() {
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void run() {
+                    fn_getlocation();
+                }
+            });
+
+        }
+    }
+
+    private void fn_update(Location location){
+        intent.putExtra("latutide",location.getLatitude()+"");
+        intent.putExtra("longitude",location.getLongitude()+"");
+        Log.d(TAG, "Location update => Longitude : " + location.getLatitude()+ " Latutide : "+ location.getLongitude());
+        Toast.makeText(this, "Location update => Longitude : "+ location.getLatitude()+ " Latutide : "+ location.getLongitude(), Toast.LENGTH_SHORT).show();
+        sendBroadcast(intent);
+    }
+
+
+}
